bc273224f92bf2b3c8705e1ae041929d13dec3d2
Claudio Hollman <claudiohollman@gmail.com>
2023-08-21 18:21:10 -0300

    Agrego Prettier y React Icons

--- src/components/shared-layout/SharedLayout.js
+++ src/components/shared-layout/SharedLayout.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Navbar } from '../../components'
 
 const SharedLayout = (props) => {
   return (
     <div>
+      <Navbar />
       <Outlet />
     </div>
-  );
-};
+  )
+}
 
-export default SharedLayout;
+export default SharedLayout
