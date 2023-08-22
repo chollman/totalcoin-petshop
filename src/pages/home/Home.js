24dffbb0e7c9b7dc78c4220f1384723582de8041
Claudio Hollman <claudiohollman@gmail.com>
2023-08-22 12:48:24 -0300

    Agrego disclaimer home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,8 +2,11 @@ import { MdOutlinePets } from 'react-icons/md'
 
 function Home() {
   return (
-    <div className='home-icon'>
-      <MdOutlinePets />
+    <div>
+      <p>Por favor, registrate o iniciá sesión para poder usar la App.</p>
+      <div className='home-icon'>
+        <MdOutlinePets />
+      </div>
     </div>
   )
 }
